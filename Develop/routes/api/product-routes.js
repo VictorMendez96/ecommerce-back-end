3e96acf6956cf792160ddc6f3cc23f4902e56a1d
V Mendez <victormendez990@gmail.com>
2022-10-23 20:39:53 -0600

    Create try/catch frame to copy to other api routes

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -7,6 +7,11 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 // get one product
