35c427758fbc4a5debefc4b2243d95dda455dd57
V Mendez <victormendez990@gmail.com>
2022-10-23 20:30:14 -0600

    Start category routes, start error catching

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,48 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try{
+    const allCats = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).JSON(allCats)
+  }
 });
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try{
+    const oneCat = await Category.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{ model: Product }]
+    });
+    res.status(200).JSON(oneCat)
+  }
 });
 
 router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCat = await Category.create(req.body);
+    res.status(200).JSON(newCat)
+  }
 });
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+
+  }
 });
 
 router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    
+  }
 });
 
 module.exports = router;
